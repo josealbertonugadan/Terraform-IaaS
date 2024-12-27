dbe1a1d44f2880220dcdb8da48e3efbbd1d0166f
José Alberto Núñez Magadán <novalis.janm@gmail.com>
2024-12-27 12:28:15 -0600

    Format Terraform configuration for consistency and readability

--- mi-primer-servidor/main.tf
+++ mi-primer-servidor/main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 data "aws_vpc" "default" {
-    default = true
+  default = true
 }
 
 resource "aws_instance" "mi_servidor" {
-    ami = "ami-005fc0f236362e99f"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
 
-user_data = <<-EOF
+  user_data = <<-EOF
             #!/bin/bash
             apt-get update
             apt-get install -y busybox-static
@@ -19,19 +19,19 @@ user_data = <<-EOF
             nohup busybox httpd -f -p 8080 &
             EOF
 
-    tags = {
-        Name = "Mi primer servidor"      
-    }
+  tags = {
+    Name = "Mi primer servidor"
+  }
 }
 
 resource "aws_security_group" "mi_grupo_de_seguridad" {
-    name   = "primer-servidor-sg"
-    vpc_id = data.aws_vpc.default.id
-    ingress {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Acceso al puerto 8080 desde el exterior"
-        from_port   = 8080
-        to_port     = 8080
-        protocol    = "TCP"
-    }
+  name   = "primer-servidor-sg"
+  vpc_id = data.aws_vpc.default.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Acceso al puerto 8080 desde el exterior"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+  }
 }
--- mi-primer-servidor/outputs.tf
+++ mi-primer-servidor/outputs.tf
@@ -1,4 +1,4 @@
 output "dns_publica" {
-    description = "DNS pública del servidor"
-    value       = "http://${aws_instance.mi_servidor.public_dns}:8080"
+  description = "DNS pública del servidor"
+  value       = "http://${aws_instance.mi_servidor.public_dns}:8080"
 }
